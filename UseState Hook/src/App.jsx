fe2dfaa3b5518990169dc42aff4730050bd79c16
GITA <jgitanjali592@gmail.com>
2024-04-17 13:09:16 +0530

    first commit

---
+++ UseState Hook/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import UseStateHook from './Components/Use-State-Hook'
+
+function App() {
+    return(
+     <div>
+
+       <h2>Hello word</h2>
+       <UseStateHook/> 
+     </div>
+    ) 
+
+      
+
+}
+
+export default App;
