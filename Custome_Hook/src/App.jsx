f2954da5a602f9917f483e073ae127c4871e7aea
GITA <jgitanjali592@gmail.com>
2024-05-06 13:11:27 +0530

    this code for windowsize change

--- Custome_Hook/src/App.jsx
+++ Custome_Hook/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Counter from './components/Counter';
+import useWindowSize from './hooks/use-window';
 
 const App = () => {
+
+  const [width, height] = useWindowSize();
   return (
     <div>
+      {/* custome hooks in react are functions that allow you to extract and reuse statefull logic components */}
+
+     <h5>
+      question-1:build a custon hook to manage and update a counter  with appropriate functionalities
+     </h5>
       <Counter />
+
+      <h5>build a custome hooks to efficiently track and update   window dimentions dynamically</h5>
+         <p>Width:{width}</p>
+         <p>Height:{height}</p>
     </div>
   );
 };
